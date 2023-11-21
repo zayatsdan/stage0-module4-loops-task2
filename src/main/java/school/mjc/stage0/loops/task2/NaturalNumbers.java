d13049e915befcbd4528ee848b2d381483b9d154
zayatsdan <36342659+zayatsdan@users.noreply.github.com>
2023-11-21 17:28:45 +0100

    Update NaturalNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/NaturalNumbers.java
@@ -2,5 +2,10 @@ package school.mjc.stage0.loops.task2;
 
 public class NaturalNumbers {
     public void naturalNumbersPrinter(int lastPrinted) {
+        int counter=0;
+        while (counter<=lastPrinted){
+            System.out.println(counter);
+            counter++;
+        }
     }
 }
