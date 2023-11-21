db83f0643ce0db8fdbbf38443539f66f2e2448ef
zayatsdan <36342659+zayatsdan@users.noreply.github.com>
2023-11-21 17:30:54 +0100

    Update FactorialNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/FactorialNumbers.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class FactorialNumbers {
     public void printFactorialRow(int printToInclusive) {
+         int counter=1;
+        int lastFact=1;
+        while (counter<=printToInclusive){
+            System.out.println(counter*lastFact);
+            lastFact=counter*lastFact;
+            counter++;
+        }
 
     }
 }
