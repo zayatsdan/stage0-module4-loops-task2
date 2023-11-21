dc7ff92e7801cdae21b646c00810cdd53a5114bc
zayatsdan <36342659+zayatsdan@users.noreply.github.com>
2023-11-21 17:28:18 +0100

    Update WordsBuilder.java

--- src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
+++ src/main/java/school/mjc/stage0/loops/task2/WordsBuilder.java
@@ -2,6 +2,14 @@ package school.mjc.stage0.loops.task2;
 
 public class WordsBuilder {
     public void buildPhrase(char... chars) {
+         StringBuilder phrase = new StringBuilder();
+
+        for (char c : chars) {
+            phrase.append(c);
+        }
+
+        System.out.println(phrase.toString());
+    }
 
     }
 }
