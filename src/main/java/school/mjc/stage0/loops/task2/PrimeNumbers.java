a29f8b3d158419a222ce08ee47c4bcf98a983950
zayatsdan <36342659+zayatsdan@users.noreply.github.com>
2023-11-21 17:30:33 +0100

    Update PrimeNumbers.java

--- src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
+++ src/main/java/school/mjc/stage0/loops/task2/PrimeNumbers.java
@@ -2,5 +2,27 @@ package school.mjc.stage0.loops.task2;
 
 public class PrimeNumbers {
     public void printPrimeNumbers(int printToInclusive) {
+        int counter=1;
+        int step=1;
+        int notSim=0;
+        while(counter<=printToInclusive){
+            while (step<counter){
+                if(counter%step!=0){
+                    step++;
+                }
+                else {
+                    notSim++;
+                    break;
+                }
+            }
+            if(notSim==0){
+                System.out.println(counter);
+            }
+            else {
+                notSim=0;
+            }
+            step=2;
+            counter++;
+        }
     }
 }
